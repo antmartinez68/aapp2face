742706f6311c21b0e8d101c16d5d8da67703580a
Antonio Martínez <77786183+antmartinez68@users.noreply.github.com>
2023-02-18 22:51:36 +0100

    Añade gestionar solicitudes anulación facturas en CLI

--- aapp2face/cli/anulaciones.py
+++ aapp2face/cli/anulaciones.py
@@ -9,6 +9,10 @@ from typing import Optional
 import typer
 
 from aapp2face import exceptions
+from aapp2face.lib.objects import (
+    GestionarSolicitudAnulacionFactura,
+    PeticionSolicitudAnulacionListadoFactura,
+)
 
 from .helpers import err_rprint, export_data, rprint, verify_export
 
@@ -69,3 +73,72 @@ def nuevas(
             print("")
 
     rprint(f"[info]{len(facturas)}[/info] nuevas solicitudes de anulación")
+
+
+@app.command()
+def gestionar(
+    ctx: typer.Context,
+    oficina_contable: str = typer.Argument(
+        ...,
+        show_default=False,
+        help="Código DIR3 de la Oficina Contable.",
+    ),
+    codigo: str = typer.Argument(
+        ...,
+        show_default=False,
+        help="Identificador del código de estado a asignar.",
+    ),
+    comentario: str = typer.Argument(
+        ...,
+        show_default=False,
+        help="Comentario asociado al cambio de estado.",
+    ),
+    numeros_registro: list[str] = typer.Argument(
+        ...,
+        show_default=False,
+        help="Números de registro de las facturas con solicitud de anulación.",
+    ),
+):
+    """Gestiona las solicitudes de anulación de facturas.
+
+    Si se indican varias facturas, todas ellas deben pertenecer a la
+    misma Oficina Contable. El nuevo estado y comentario facilitados
+    serán asignados a todas las facturas indicadas. Obsérvese que el
+    parámetro comentario es obligatorio. Si se desea dejar en blanco se
+    de indicar explícitamente, por ejemplo, usando comillas ("").
+    """
+
+    peticiones: list[PeticionSolicitudAnulacionListadoFactura] = []
+    for numero_registro in numeros_registro:
+        peticion = PeticionSolicitudAnulacionListadoFactura(
+            oficina_contable, numero_registro, codigo, comentario
+        )
+        peticiones.append(peticion)
+
+    try:
+        confirmaciones: GestionarSolicitudAnulacionFactura = (
+            ctx.obj.face_connection.gestionar_solicitud_anulacion_listado_facturas(
+                peticiones
+            )
+        )
+    except exceptions.FACeManagementException as exc:
+        err_rprint(f"[error]Error {exc.code}:[/error] {exc.msg}.")
+        raise typer.Exit(4)
+
+    errors = 0
+    for confirmacion in confirmaciones:
+        if isinstance(confirmacion, GestionarSolicitudAnulacionFactura):
+            rprint(
+                f"[field]Número de registro:[/field] [info]{confirmacion.numero_registro}[/info]"
+            )
+            rprint(f"[field]Código de estado:[/field]   {confirmacion.codigo}")
+        else:
+            rprint(f"[field]Número de registro:[/field] [info]{confirmacion.id}[/info]")
+            rprint(
+                f"  [error]Error:[/error] {confirmacion.codigo} {confirmacion.descripcion}."
+            )
+            errors += 1
+        print()
+    rprint(
+        f"[info]{len(confirmaciones)-errors}[/info] cambios correctos y [error]{errors}[/error] errores."
+    )
--- tests/test_cli_anulaciones.py
+++ tests/test_cli_anulaciones.py
@@ -47,3 +47,44 @@ def test_nuevas_anulaciones_oficina_no_existente():
     )
     assert result.exit_code == 4
     assert expected_output.replace("\n", "") in result.stdout.replace("\n", "")
+
+
+def test_gestionar_solicitud_anulacion_listado_facturas():
+    oficina_contable = "P00000010"
+    codigo = "4500"
+    comentario = ""
+    numero_registro_1 = "202001029111"
+    numero_registro_2 = "202001019122"
+    numero_registro_3 = "9999"
+    expected_output = (
+        "Aviso: Usando entorno de simulación. Algunos parámetros de configuración serán ignorados."
+        "Número de registro: 202001029111"
+        f"Código de estado:   {codigo}"
+        ""
+        "Número de registro: 202001019122"
+        "  Error: 505 Esta transición no esta permitida a través de este web service."
+        ""
+        "Número de registro: 9999"
+        "  Error: 501 No se han encontrado facturas asociadas de la oficina contable al RCF."
+        ""
+        "1 cambios correctos y 2 errores."
+    )
+
+    result = runner.invoke(
+        app,
+        [
+            "--fake-set",
+            TEST_RESPONSES_PATH,
+            "anulaciones",
+            "gestionar",
+            oficina_contable,
+            codigo,
+            comentario,
+            numero_registro_1,
+            numero_registro_2,
+            numero_registro_3,
+        ],
+    )
+
+    assert result.exit_code == 0
+    assert expected_output.replace("\n", "") in result.stdout.replace("\n", "")
